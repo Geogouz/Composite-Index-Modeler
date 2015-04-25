e9f2db5631890de9e0330dd39bd1d80d65f9774f
Dimitris Xenakis <sparkdevelopment.social@gmail.com>
2015-04-25 19:39:11 +0300

    some Window State added

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ __author__ = 'Dimitris Xenakis'
 print "adding", __name__
 
 import os
+import platform
 import threading
 import time
 from datetime import datetime
 import urllib
 import json
+
+from kivy.config import Config
+
+# Reads the app's config. If not, defaults are loaded.
+Config.read('cimgui.ini')
 
 from kivy.core.window import Window
 from kivy.app import App
@@ -16,19 +22,19 @@ from kivy.animation import Animation
 from kivy.factory import Factory
 from kivy.properties import BooleanProperty
 
-# set WorldBank API static parameters
+# Set WorldBank API static parameters.
 start_url = "http://api.worldbank.org/"
 end_url = "?per_page=30000&format=json"
 
-# set url catalogs
+# Set url catalogs.
 countries = "countries/"
 topics = "topics/"
 indicators = "indicators/"
 
-# set_coredb
+# Prepare the file to store core's index database.
 coredb_py = None
 
-# set user.db
+# Set userdb
 userdb = [["GRC", "ALB", "ITA", "TUR", "CYP"],
           ["SP.DYN.LE00.IN", "MYS.MEA.YSCH.25UP.MF", "SE.SCH.LIFE", "NY.GNP.PCAP.PP.CD", "UNDP.HDI.XD"]]
 
@@ -37,13 +43,37 @@ userdb = [["GRC", "ALB", "ITA", "TUR", "CYP"],
 
 class MainWindow(BoxLayout):
 
-    # set kivy properties. no process or popup is running on app init
+    # Prepare kivy properties that show if a process or a popup are currently running. Set to False on app's init.
     processing = BooleanProperty(False)
     popup_active = BooleanProperty(False)
 
+    # This method can generate new threads, so that main thread (GUI) won't get frozen.
     def threadonator(self, *arg):
         threading.Thread(target=arg[0], args=(arg,)).start()
         return 1
+
+    # This method addresses our window's state changes, to their specific handling actions.
+    def windows_state(self, state):
+        if state == "minimize":
+            Window.minimize()
+        elif state == "toggle_fullscreen":
+            Window.toggle_fullscreen()
+        elif state == "close":
+            App.get_running_app().stop_warning()
+        else:
+            print "Unknown Windows State"
+        return 1
+
+
+    def config_state(self, state):
+        config = App.get_running_app().read_config()
+        config.set('graphics', 'fullscreen', 1)
+        config.write()
+
+    def config_state_delete(self, state):
+        config = App.get_running_app().read_config()
+        config.set('graphics', 'fullscreen', 0)
+        config.write()
 
     def update_progress(self, *arg):
         anim_bar = Factory.AnimWidget()
@@ -57,13 +87,14 @@ class MainWindow(BoxLayout):
         self.core_build_progress_bar.remove_widget(anim_bar)
         return 1
 
-    # build coredb with indicators and countries
+    # This method builds core's index database with indicators and countries.
     def core_build(self, *arg):
-        # init process
+
+        # A process just started running (in a new thread).
         self.processing = True
         self.threadonator(self.update_progress)
 
-        # Try, in case there is a problem with the update process
+        # Try, in case there is a problem with the online updating process.
         try:
             time.sleep(4)
             """
@@ -160,23 +191,23 @@ class MainWindow(BoxLayout):
         self.processing = False
         return 1
 
-    # check for last coredb update
+    # This method checks for last core's index database update.
     def check(self, *arg):
         global coredb_py
 
         while self.popup_active:
-            # if there is any process running, wait until finish
+            # If there is any process running, wait until finish.
             while self.processing:
                 time.sleep(1)
 
-            # try to open the json DB file
+            # Try to open the json DB file.
             try:
                 stored_coredb = open("./DB/core.db", "r")
 
-                # convert json file into temp python structure
+                # Convert json file into temp python structure.
                 coredb_py = json.load(stored_coredb)
 
-                # close json file
+                # Close json file.
                 stored_coredb.close()
 
                 self.coredb_state.text = ("Latest DB Update:\n" + coredb_py[0]['table_date'])
@@ -184,24 +215,65 @@ class MainWindow(BoxLayout):
                 self.coredb_state.text = "No valid Indices Database found!\nPlease update it."
 
             time.sleep(5)
-        return
-
-    # build valuesdb with indicators and countries
-    def values_build(self):
-        return "test function"
+        return 1
 
 
 class CIMgui(App):
 
+    def build_config(self, cim_config):
+        cim_config.setdefaults('System Info',
+                               {'usr_os': platform.system(),
+                                'arch': platform.machine(), 'py': platform.python_version()})
+
+        cim_config.setdefaults('graphics',
+                               {'fullscreen': 0,
+                                'height': 768,
+                                'width': 1280,
+                                'position': 'custom',
+                                'top': 100, 'left': 100})
+
+#    def build_settings(self, settings):
+#        settings.add_json_panel("CIM Settings", self.config, data="""
+#        [{"type": "options",
+#        "title": "System Info",
+#        "section": "System Info",
+#        "key": "usr_os",
+#        "options": ["1", "2"]}]""")
+
+    def read_config(self):
+        return self.config
+
     def stop_warning(self, *args, **kwargs):
         print "Are you sure?.."
-        return True
+        Window.close()
 
+    def just_clicked_here(self, *args):
+        print "just clicked here", args
+        #print self.position
+
+    def moving_cursor(self, *args):
+        print "moving", args
+
+    def mouse_position(self, *args):
+        self.position = args[1]
+        #print self.position
+        #print self.collide_point(*args[1])
+        """
+        if self.hovered == inside:
+            return
+        self.hovered = inside
+        """
+
+    # This function prepares the window.
     def build(self):
-        #Window.borderless = True # halted until bug fixed
+        self.use_kivy_settings = False
+        Window.borderless = True
         Window.bind(on_request_close=self.stop_warning)
+        Window.bind(mouse_pos=self.mouse_position)
+        Window.bind(on_touch_down=self.just_clicked_here)
+        Window.bind(on_touch_move=self.moving_cursor)
         return MainWindow()
 
-# must be called from main
+# Must be called from main.
 if __name__ == "__main__":
     CIMgui().run()
